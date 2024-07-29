a4701dfb04b08e7a1f5673040c0d691db48f5230
auto01data <156892832+auto01data@users.noreply.github.com>
2024-07-29 06:00:41 +0000

    modifications on home.html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hall_world():
-  return "<h1>Hello World</h1>"
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
